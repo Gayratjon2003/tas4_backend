dd02f5a0b49d6d80d2062755968e8ae109914cf9
Gayratjon2003 <gayratjonabdijobborov@gmail.com>
2023-05-28 17:41:01 +0500

    static mongodb link added

--- startup/db.js
+++ startup/db.js
@@ -3,7 +3,7 @@ const winston = require("winston");
 const config = require("config");
 module.exports = async function () {
   try {
-    await mongoose.connect(config.get("db"), {
+    await mongoose.connect("mongodb+srv://gayratjon2003:@Super2003@cluster0.uwrruyk.mongodb.net/task4", {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
